60ae07a9fd67a904d7bf3ef54184af03e6948767
Mushka Nikita <nikita.mushka@onlyoffice.com>
2023-12-16 03:09:24 +0500

    added people table

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,23 @@
 import Table from '../Table';
-import { fileData } from '../mock';
+import { fileData, peopleData } from '../mock';
 
 function App() {
   return (
-    <div style={{ width: 'calc(100vw - 200px)', height: '200px' }}>
-      <Table id={'table_mydocuments'} itemTypeName='file' data={fileData} />
-    </div>
+    <>
+      <div style={{ width: 'calc(100vw - 200px)', height: '200px' }}>
+        <Table id={'table_mydocuments'} itemTypeName='file' data={fileData} />
+      </div>
+
+      <div style={{ height: '200px' }}></div>
+
+      <div style={{ width: 'calc(100vw - 200px)', height: '200px' }}>
+        <Table
+          id={'table_accounts_people'}
+          itemTypeName='user'
+          data={peopleData}
+        />
+      </div>
+    </>
   );
 }
 
